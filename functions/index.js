5fe937164f413bc0fee0ea2e27fe98dbb9dab4c3
Flieris <lenniuch@hotmail.com>
2017-12-14 16:50:47 +0100

    function implementation

--- functions/index.js
+++ functions/index.js
@@ -16,22 +16,46 @@ class Point {
 exports.pointSanitization = functions.database.ref().onWrite(event => { // '/{mmc}/{operatorName}/{networkType}/{cellId}'
   var abc = admin.database().ref('260') // <-
   abc.once('value', function (userSnapshot) {
-    userSnapshot.child('Orange').forEach(function (networkSnapshot) {
-      networkSnapshot.forEach(function (cellSnapshot) {
-        var dataJSON = cellSnapshot.toJSON()
-        // console.log(dataJSON)
-        // console.log(cellSnapshot.key)
-        // iterate through data and find lat and lng matching points
-        // calculate mean power of those points and leave only one point
-        // for specific geographical location
-        for (var item in dataJSON) {
-          var point1 = new Point(dataJSON[item].cellId, dataJSON[item].cellType, dataJSON[item].lac, dataJSON[item].latitude, dataJSON[item].longitude, dataJSON[item].signalStrength)
-          for (var jtem = item in dataJSON) {
-            if ((dataJSON[jtem].cellId === point1.cellId) && (dataJSON[jtem].latitude === point1.lat) && (dataJSON[jtem].longitude === point1.lng)) {
-              console.log(point1)
+    userSnapshot.forEach(function (operatorSnapshot) {
+      operatorSnapshot.forEach(function (networkSnapshot) {
+        networkSnapshot.forEach(function (cellSnapshot) {
+          var dataJSON = cellSnapshot.toJSON()
+          for (var item in dataJSON) {
+            var point1 = new Point(dataJSON[item].cellId, dataJSON[item].cellType, dataJSON[item].lac, dataJSON[item].latitude, dataJSON[item].longitude, dataJSON[item].signalStrength)
+
+            if (point1.cellType === undefined) {
+              point1.cellType = 'UNKNOWN'
+            }
+            var tab = []
+            for (var jtem = item in dataJSON) {
+              if ((dataJSON[jtem].latitude === point1.lat) && (dataJSON[jtem].longitude === point1.lng)) {
+                tab.push(dataJSON[jtem].signalStrength)
+                // console.log(jtem)
+                var db = admin.database()
+                var ref2 = db.ref(abc.key + '/' + operatorSnapshot.key + '/' + networkSnapshot.key + '/' + cellSnapshot.key + '/' + jtem)
+                ref2.remove()
+                // console.log(abc.child(jtem).key)
+              }
+            }
+            if (tab.length > 0) {
+              var sum = point1.signal
+              for (var i = 0; i < tab.length; i++) {
+                sum = sum + tab[i]
+              }
+              var mean = sum / (tab.length + 1)
+              var db2 = admin.database()
+              var refe = db2.ref(abc.key + '/' + operatorSnapshot.key + '/' + networkSnapshot.key + '/' + cellSnapshot.key + '/' + item)
+              refe.update({
+                cellId: point1.cellId,
+                cellType: point1.cellType,
+                lac: point1.lac,
+                latitude: point1.lat,
+                longitude: point1.lng,
+                signalStrength: mean
+              })
             }
           }
-        }
+        })
       })
     })
   })
