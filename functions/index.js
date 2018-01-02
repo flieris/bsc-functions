12fd1efa4cd9b210f1820b0e4c2655e2deb476ad
Flieris <lenniuch@hotmail.com>
2018-01-02 13:35:57 +0100

    removed points function and added locpoint function

--- functions/index.js
+++ functions/index.js
@@ -13,51 +13,122 @@ class Point {
     this.signal = signal
   }
 }
-exports.pointSanitization = functions.database.ref().onWrite(event => { // '/{mmc}/{operatorName}/{networkType}/{cellId}'
-  var abc = admin.database().ref('260') // <-
-  abc.once('value', function (userSnapshot) {
-    userSnapshot.forEach(function (operatorSnapshot) {
-      operatorSnapshot.forEach(function (networkSnapshot) {
-        networkSnapshot.forEach(function (cellSnapshot) {
+exports.locpoint = functions.database.ref().onCreate(event => { // '/{mcc}/{operator}/{type}/{cellId}/{pushId}'
+  const original = event.data
+  var point1, mcc, operator, type
+  original.forEach(function (mccSnapshot) {
+    mcc = mccSnapshot.key
+    mccSnapshot.forEach(function (operatorSnapshot) {
+      operator = operatorSnapshot.key
+      operatorSnapshot.forEach(function (typeSnapshot) {
+        type = typeSnapshot.key
+        typeSnapshot.forEach(function (cellSnapshot) {
           var dataJSON = cellSnapshot.toJSON()
           for (var item in dataJSON) {
-            var point1 = new Point(dataJSON[item].cellId, dataJSON[item].cellType, dataJSON[item].lac, dataJSON[item].latitude, dataJSON[item].longitude, dataJSON[item].signalStrength)
+            point1 = new Point(dataJSON[item].cellId, dataJSON[item].cellType, dataJSON[item].lac, dataJSON[item].latitude, dataJSON[item].longitude, dataJSON[item].signalStrength)
 
             if (point1.cellType === undefined) {
               point1.cellType = 'UNKNOWN'
-            }
-            var tab = []
-            for (var jtem = item in dataJSON) {
-              if ((dataJSON[jtem].latitude === point1.lat) && (dataJSON[jtem].longitude === point1.lng)) {
-                tab.push(dataJSON[jtem].signalStrength)
-                // console.log(jtem)
-                var db = admin.database()
-                var ref2 = db.ref(abc.key + '/' + operatorSnapshot.key + '/' + networkSnapshot.key + '/' + cellSnapshot.key + '/' + jtem)
-                ref2.remove()
-                // console.log(abc.child(jtem).key)
-              }
-            }
-            if (tab.length > 0) {
-              var sum = point1.signal
-              for (var i = 0; i < tab.length; i++) {
-                sum = sum + tab[i]
-              }
-              var mean = sum / (tab.length + 1)
-              var db2 = admin.database()
-              var refe = db2.ref(abc.key + '/' + operatorSnapshot.key + '/' + networkSnapshot.key + '/' + cellSnapshot.key + '/' + item)
-              refe.update({
-                cellId: point1.cellId,
-                cellType: point1.cellType,
-                lac: point1.lac,
-                latitude: point1.lat,
-                longitude: point1.lng,
-                signalStrength: mean
-              })
             }
           }
         })
       })
     })
   })
+  var ref = admin.database().ref(mcc + '/' + operator + '/' + type + '/' + point1.cellId)
+  ref.once('value', function (dataSnapshot) {
+    dataSnapshot.forEach(function (pointsSnapshot) {
+      var cointained = cointainedIn(point1.lat, point1.lng, pointsSnapshot.val().latitude, pointsSnapshot.val().longitude)
+      if (cointained === true) {
+        var referenceSignal = dbmToLinear(pointsSnapshot.val().signalStrength)
+        var pointSignal = dbmToLinear(point1.signal)
+        var meanLinear = (referenceSignal + pointSignal) / 2.0
+        var ref = admin.database().ref(mcc + '/' + operator + '/' + type + '/' + point1.cellId + '/' + pointsSnapshot.key)
+        return ref.update({
+          cellId: pointsSnapshot.val().cellId,
+          cellType: pointsSnapshot.val().cellType,
+          lac: pointsSnapshot.val().lac,
+          latitude: pointsSnapshot.val().latitude,
+          longitude: pointsSnapshot.val().longitude,
+          signalStrength: linearTodBm(meanLinear)
+        })
+      }
+    })
+  })
+  // console.log(original)
   return true
 })
+
+function cointainedIn (lat1, lng1, lat2, lng2) {
+  // first check if the point is contained in circle of 5m radius around point (lat2,lng2)
+
+  var distance = calculateDistance(lat1, lng1, lat2, lng2)
+  var R = 6371e3
+  var d = 5.0 * Math.sqrt(2)
+  if (distance <= 5.0) {
+    console.log('in')
+    return true
+  } else if (distance <= d) {
+    // just check if lat and lng are withing bondaries of cornerss
+    var cphi1 = Math.asin(Math.sin(lat2 * (Math.PI / 180)) * Math.cos(d / R) + Math.cos(lat2 * (Math.PI / 180)) * Math.sin(d / R) * Math.cos(Math.PI / 4))
+    var cλ1 = (lng2 * (Math.PI / 180)) + Math.atan2(Math.sin(Math.PI / 4) * Math.sin(d / R) * Math.cos(lat2 * (Math.PI / 180)), Math.cos(d / R) - Math.sin(lat2 * (Math.PI / 180)) * Math.sin(cphi1))
+    var cphi2 = Math.asin(Math.sin(lat2 * (Math.PI / 180)) * Math.cos(d / R) + Math.cos(lat2 * (Math.PI / 180)) * Math.sin(d / R) * Math.cos(3 * Math.PI / 4))
+    var cλ2 = (lng2 * (Math.PI / 180)) + Math.atan2(Math.sin(3 * Math.PI / 4) * Math.sin(d / R) * Math.cos(lat2 * (Math.PI / 180)), Math.cos(d / R) - Math.sin(lat2 * (Math.PI / 180)) * Math.sin(cphi2))
+    var cphi3 = Math.asin(Math.sin(lat2 * (Math.PI / 180)) * Math.cos(d / R) + Math.cos(lat2 * (Math.PI / 180)) * Math.sin(d / R) * Math.cos(-3 * Math.PI / 4))
+    var cλ3 = (lng2 * (Math.PI / 180)) + Math.atan2(Math.sin(-3 * Math.PI / 4) * Math.sin(d / R) * Math.cos(lat2 * (Math.PI / 180)), Math.cos(d / R) - Math.sin(lat2 * (Math.PI / 180)) * Math.sin(cphi3))
+    var cphi4 = Math.asin(Math.sin(lat2 * (Math.PI / 180)) * Math.cos(d / R) + Math.cos(lat2 * (Math.PI / 180)) * Math.sin(d / R) * Math.cos(-Math.PI / 4))
+    var cλ4 = (lng2 * (Math.PI / 180)) + Math.atan2(Math.sin(-Math.PI / 4) * Math.sin(d / R) * Math.cos(lat2 * (Math.PI / 180)), Math.cos(d / R) - Math.sin(lat2 * (Math.PI / 180)) * Math.sin(cphi4))
+    var clat1 = cphi1 * (180 / Math.PI)
+    var clng1 = cλ1 * (180 / Math.PI)
+    var clat2 = cphi2 * (180 / Math.PI)
+    var clng2 = cλ2 * (180 / Math.PI)
+    var clat3 = cphi3 * (180 / Math.PI)
+    var clng3 = cλ3 * (180 / Math.PI)
+    var clat4 = cphi4 * (180 / Math.PI)
+    var clng4 = cλ4 * (180 / Math.PI)
+    // border conditions
+    var condition1 = lat1 <= clat1 && lng1 <= clng1
+    var condition2 = lat1 >= clat2 && lng1 <= clng2
+    var condition3 = lat1 >= clat3 && lng1 >= clng3
+    var condition4 = lat1 <= clat4 && lng1 >= clng4
+    if ((condition1 === true) && (condition2 === true) && (condition3 === true) && (condition4 === true)) {
+      console.log('in')
+      return true
+    } else {
+      console.log('out')
+      return false
+    }
+  } else {
+    console.log('out')
+    return false
+  }
+}
+
+function calculateDistance (lat1, lng1, lat2, lng2) {
+  // Haversine formula
+  var R = 6371e3
+  var phi1 = lat1 * (Math.PI / 180)
+  var phi2 = lat2 * (Math.PI / 180)
+  var dLat = (lat2 - lat1) * (Math.PI / 180)
+  var dLng = (lng2 - lng1) * (Math.PI / 180)
+
+  var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(phi1) * Math.cos(phi2) * Math.sin(dLng / 2) * Math.sin(dLng / 2)
+
+  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+
+  var d = R * c
+  // console.log(d)
+  return d
+}
+
+function dbmToLinear (dbm) {
+  var ref = 1e-3
+  var cal = Math.pow(10, dbm / 10)
+  var value = ref * cal
+  return value
+}
+
+function linearTodBm (linear) {
+  var cal = 10 * Math.log10(linear / 1e-3)
+  return cal
+}
